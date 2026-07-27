2ffbec4926cd76f52853849d82196069222d41bf
Seungmin903 <seungmin903@naver.com>
2026-07-27 11:28:21 +0900

    [Chore] Backend 기본 설정 및 소스코드 생성

---
+++ backend/Emour/src/main/java/com/ssafy/emour/EmourApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.emour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmourApplication.class, args);
+	}
+
+}
---
+++ backend/Emour/src/test/java/com/ssafy/emour/EmourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.emour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
